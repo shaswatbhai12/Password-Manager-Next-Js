33ed7422fa59db379dc00452267e51408ff4a8f0
shaswat <shaswatkumar9868@gmail.com>
2026-08-07 21:13:06 +0530

    change the theme and the toggle theme function

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,14 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/components/Navbar"
-import { ClerkProvider, Show, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
+import Navbar from "@/components/Navbar";
+import {
+  ClerkProvider,
+  Show,
+  SignInButton,
+  SignUpButton,
+  UserButton,
+} from "@clerk/nextjs";
 import { ThemeProvider } from "@/components/theme-provider";
 
 const geistSans = Geist({
@@ -23,23 +29,23 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
     <ClerkProvider>
-    <html
-      lang="en"
-      suppressHydrationWarning
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">
-          <ThemeProvider 
-              attribute="class"
-              defaultTheme="system"
-              enableSystem
-              enableColorScheme={false}
-              disableTransitionOnChange>
-          <Navbar />
-        {children}
-        </ThemeProvider>
+      <html
+        lang="en"
+        suppressHydrationWarning
+        className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      >
+        <body className="min-h-full flex flex-col">
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            
+          >
+            <Navbar />
+            {children}
+          </ThemeProvider>
         </body>
-    </html>
-        </ClerkProvider>
+      </html>
+    </ClerkProvider>
   );
 }
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,15 +1,17 @@
 "use client"
 import React from 'react'
-import { Moon, MoonIcon, Sun } from 'lucide-react'
+import { Moon, Sun } from 'lucide-react'
 import { useTheme } from 'next-themes'
 import { Button } from '@/components/ui/button'
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger} from "@/components/ui/dropdown-menu"
 import { SignInButton, Show, UserButton} from '@clerk/nextjs'
 
 const Navbar = () => {
-    const { setTheme } = useTheme()
+    const { theme, setTheme } = useTheme()
+    const toggleTheme = () => {
+        setTheme(theme === 'dark' ? 'light' : 'dark');
+    };
   return (
-    <nav className='flex justify-between items-center px-4 h-16 bg-border text-foreground'>
+    <nav className='flex justify-between items-center px-4 h-16 bg-primary/20 text-foreground'>
         <span className='font-bold text-xl'>NoPass</span>
       <ul className='flex gap-5 justify-start items-center'>
         <li>Home</li>
@@ -17,24 +19,12 @@ const Navbar = () => {
         <li>Services</li>
       </ul>
       <div className='flex justify-center gap-2 items-center'>
-        <DropdownMenu>
-            <DropdownMenuTrigger className="inline-flex h-9 w-9 items-center justify-center rounded-md border bg-background hover:bg-accent">
+            <Button className="relative" variant="outline" size="icon" onClick={toggleTheme}>
                     <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"/>
                     <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"/>
                     <span className="sr-only">Toggle Theme</span>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent align="end">
-                <DropdownMenuItem onClick={()=> setTheme("light")}>
-                    Light
-                </DropdownMenuItem>
-                <DropdownMenuItem onClick={()=> setTheme("dark")}>
-                    Dark
-                </DropdownMenuItem>
-                <DropdownMenuItem onClick={()=> setTheme("system")}>
-                    System
-                </DropdownMenuItem>
-            </DropdownMenuContent>
-        </DropdownMenu>
+            </Button>
+            
         <Show when="signed-out">
             <SignInButton mode='modal'> 
                 <Button  variant="default">
